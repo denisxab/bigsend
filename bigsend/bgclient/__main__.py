c44833af71d493e2251f9c1d903e0be221e71f41
Your Name <you@example.com>
2023-03-15 21:41:17 +0300

    "Ср 15 мар 2023 21:41:17" -

--- bigsend/bgclient/__main__.py
+++ bigsend/bgclient/__main__.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(
         description="Send a large file to a remote server."
     )
-    parser.add_argument("url", type=str, help="URL of the remote server")
+    parser.add_argument("url", type=str, help="URL of the remote server. Example: http://192.168.10.63:8083")
     parser.add_argument("file_path", type=str, help="Path to the local file to be sent")
     parser.add_argument(
         "remote_path",
